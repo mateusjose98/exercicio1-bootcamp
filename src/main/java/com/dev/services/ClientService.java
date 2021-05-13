5468bb8e3c46b1a2965403acc30b3cce2e5624bd
José Mateus <josemateus.ufma@gmail.com>
2021-05-13 16:34:39 -0300

    Put and Delete

--- src/main/java/com/dev/services/ClientService.java
+++ src/main/java/com/dev/services/ClientService.java
@@ -3,10 +3,14 @@ package com.dev.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.dto.ClientDTO;
 import com.dev.entities.Client;
@@ -39,11 +43,41 @@ public class ClientService {
 		return new ClientDTO(client);
 	}
 
+	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client client = repository
 				.save(new Client(null, dto.getName(), dto.getCpf(), dto.getIncome(), dto.getBirthDate(), dto.getChildren()));
 		
 		return new ClientDTO(client);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+			
+		}catch (EntityNotFoundException e){
+			throw new ClientNotFoundException("Id not found");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ClientNotFoundException("Id not found");
+		} 
+		
 	}
 	
 	
